141dda14cef1efcd50b3c64551e7e68e1d3b4e93
Dmitriy Rudnik <dmitriyrudnik@gmail.com>
2024-11-16 16:51:19 +0200

    add public commands

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,9 @@ import {downloadTorrent, searchToloka} from './toloka.ts'
  *  - SYNOLOGY_HOST
  *  - SYNOLOGY_USER
  *  - SYNOLOGY_PASSWORD
+ *  - TOLOKA_USERNAME
+ *  - TOLOKA_PASSWORD
+ *  - PUBLIC_USERNAMES
  */
 
 type BotContext = Context & ConversationFlavor
@@ -22,7 +25,108 @@ process.once('SIGTERM', bot.stop)
 bot.use(session({initial: () => ({})}))
 bot.use(conversations())
 
-// is valid user
+const commandsMenu: BotCommand[] = []
+const addCommand = (id: string, label: string, middleware: CommandMiddleware<BotContext>) => {
+  bot.command(id, middleware)
+  commandsMenu.push({
+    command: id,
+    description: label,
+  })
+}
+
+bot.use((ctx, next) => {
+  const publicUsers = (process.env.PUBLIC_USERNAMES || '').split(',')
+  if (ctx.chat?.type === 'private' && [process.env.OWNER_USERNAME, ...publicUsers].includes(ctx.chat.username!)) {
+    return next()
+  } else {
+    return ctx.reply('🚩 You dont have permission to use this command.')
+  }
+})
+
+async function searchTolokaConversation(conversation: Conversation<BotContext>, ctx: BotContext) {
+  await ctx.reply('Search query?')
+  const {message} = await conversation.wait()
+  if (!message?.text) {
+    await ctx.reply('Error: expect text here')
+    return
+  }
+  const query = message.text
+  const results = await conversation.external(() => searchToloka(query))
+  if (results.length === 0) {
+    await ctx.reply('Empty results')
+    return
+  }
+
+  await ctx.reply(
+    results
+      .map((i, index) => `*\[${index}\]* - ${i.title}`)
+      .join('\n\n'),
+    {
+      parse_mode: 'Markdown',
+      reply_markup: {
+        inline_keyboard: [
+          results.map((_, index) => ({
+            text: index.toString(),
+            callback_data: index.toString()
+          })),
+          [{
+            text: '⬅️',
+            callback_data: '__back'
+          }]
+        ]
+      }
+    }
+  )
+  const searchSelectAction = await conversation.waitFor('callback_query:data')
+  const selectedAction = searchSelectAction.callbackQuery.data
+
+  if (selectedAction === '__back') {
+    await ctx.reply('Cancel')
+    return
+  }
+
+  const selectedIndex = +selectedAction
+  const resultItem = results[selectedIndex]!
+  await ctx.reply(`You choose, [${selectedIndex}], ${resultItem.title}`)
+
+  const file = await downloadTorrent(resultItem.url)
+
+  const fileId = (await ctx.replyWithDocument(
+    new InputFile(file as any, 'file.torrent')
+  )).document!.file_id
+
+  return [resultItem.title, fileId] as const
+}
+
+async function searchTolokaAndDownloadNas(conversation: Conversation<BotContext>, ctx: BotContext) {
+  const searchResult = await searchTolokaConversation(conversation, ctx)
+  if (!searchResult) return
+
+  const [resultItemTitle, fileId] = searchResult
+  const folder = await chooseFolder(conversation, ctx)
+  if (!folder) {
+    return ctx.reply('Cancel')
+  }
+  const filePath = (await ctx.api.getFile(fileId)).file_path!
+  await createDownloadTask(folder, getFileUrl(filePath))
+  await ctx.reply(`👌, Start download ${resultItemTitle} into "${folder}"`)
+}
+
+bot.use(
+  createConversation<BotContext>(
+    searchTolokaConversation
+  )
+)
+
+bot.use(
+  createConversation<BotContext>(
+    searchTolokaAndDownloadNas
+  )
+)
+
+addCommand('search_toloka', 'Search toloka', ({conversation}) => conversation.enter('searchTolokaConversation'))
+
+// next commands should be private
 bot.use((ctx, next) => {
   if (ctx.chat?.type === 'private' && ctx.chat.username === process.env.OWNER_USERNAME) {
     return next()
@@ -59,15 +163,6 @@ const chooseFolder = async (conversation: Conversation<BotContext>, ctx: BotCont
   return cbData
 }
 
-const commandsMenu: BotCommand[] = []
-const addCommand = (id: string, label: string, middleware: CommandMiddleware<BotContext>) => {
-  bot.command(id, middleware)
-  commandsMenu.push({
-    command: id,
-    description: label
-  })
-}
-
 addCommand('status', 'Status', async (ctx) => {
   const tasks = await getTasks()
   if (tasks.length === 0)
@@ -85,71 +180,7 @@ addCommand('clean', 'Clean completed', async (ctx) => {
   await ctx.reply('🧹')
 })
 
-bot.use(
-  createConversation<BotContext>(
-    async function tolokaSearch(conversation, ctx) {
-      await ctx.reply('Search query?')
-      const {message} = await conversation.wait()
-      if (!message?.text) {
-        await ctx.reply('Error: expect text here')
-        return
-      }
-      const query = message.text
-      const results = await conversation.external(() => searchToloka(query))
-      if (results.length === 0) {
-        await ctx.reply('Empty results')
-        return
-      }
-
-      await ctx.reply(
-        results
-          .map((i, index) => `*\[${index}\]* - ${i.title}`)
-          .join('\n\n'),
-        {
-          parse_mode: 'Markdown',
-          reply_markup: {
-            inline_keyboard: [
-              results.map((i, index) => ({
-                text: index.toString(),
-                callback_data: index.toString()
-              })),
-              [{
-                text: '⬅️',
-                callback_data: '__back'
-              }]
-            ]
-          }
-        }
-      )
-      const searchSelectAction = await conversation.waitFor('callback_query:data')
-      const selectedAction = searchSelectAction.callbackQuery.data
-
-      if (selectedAction === '__back') {
-        await ctx.reply('Cancel')
-        return
-      }
-
-      const selectedIndex = +selectedAction
-      const resultItem = results[selectedIndex]!
-      await ctx.reply(`You choose, [${selectedIndex}], ${resultItem.title}`)
-
-      const folder = await chooseFolder(conversation, ctx)
-      if (!folder) {
-        return ctx.reply('Cancel')
-      }
-      const file = await downloadTorrent(resultItem.url)
-
-      const fileId = (await ctx.replyWithDocument(
-        new InputFile(file as any, 'file.torrent')
-      )).document!.file_id
-      const filePath = (await ctx.api.getFile(fileId)).file_path!
-      await createDownloadTask(folder, getFileUrl(filePath))
-      await ctx.reply(`👌, Start download ${resultItem.title} into "${folder}"`)
-    },
-  )
-)
-
-addCommand('toloka', 'Search toloka', ({conversation}) => conversation.enter('tolokaSearch'))
+addCommand('search_toloka_nas', 'Search toloka NAS', ({conversation}) => conversation.enter('searchTolokaAndDownloadNas'))
 
 bot.use(
   createConversation<BotContext>(
